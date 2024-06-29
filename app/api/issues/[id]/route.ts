d410db28a02e6bb8965bee120f3fb482ad6f9acd
Shuwen Wu <wu000317@algonquinlive.com>
2024-06-29 15:38:39 -0400

    Build an API for deleting issues

--- app/api/issues/[id]/route.ts
+++ app/api/issues/[id]/route.ts
@@ -1,5 +1,6 @@
 import { issueSchema } from "@/app/validationSchemas";
 import prisma from "@/prisma/client";
+import { notFound } from "next/navigation";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function PATCH(
@@ -25,4 +26,19 @@ export async function PATCH(
     },
   });
   return NextResponse.json(updatedIssue);
+}
+
+export async function DELETE(
+  request: NextRequest,
+  { params }: { params: { id: string } }
+) {
+  const foundIssue = await prisma.issue.findUnique({
+    where: { id: parseInt(params.id) },
+  });
+  if (!foundIssue)
+    return NextResponse.json({ error: "Invalid issue" }, { status: 404 });
+  await prisma.issue.delete({
+    where: { id: foundIssue.id },
+  });
+  return NextResponse.json({});
 }
