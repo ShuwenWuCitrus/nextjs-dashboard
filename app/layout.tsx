45c084f74df54b1ca8f024b6ba4a240805fba2c6
Shuwen Wu <wu000317@algonquinlive.com>
2024-06-29 15:22:32 -0400

    Add a delete btn

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,7 +6,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import NavBar from "./NavBar";
 import "@radix-ui/themes/styles.css";
-import { Theme, ThemePanel } from "@radix-ui/themes";
+import { Container, Theme, ThemePanel } from "@radix-ui/themes";
 
 const inter = Inter({ subsets: ["latin"], variable: "--font-inter" });
 
@@ -25,7 +25,9 @@ export default function RootLayout({
       <body className={inter.variable}>
         <Theme accentColor="iris">
           <NavBar />
-          <main className="p-5">{children}</main>
+          <main className="p-5">
+            <Container>{children}</Container>
+          </main>
         </Theme>
       </body>
     </html>
